32d4843f2f5be0c57ab301d094abf5126db7e636
Alex <e_fridman@mail.ru>
2024-11-08 19:04:14 +0300

    07/11

--- Intro/module_0_1.py
+++ Intro/module_0_1.py
@@ -1,2 +1,30 @@
-print ('Hello world')
-print ('Hello' * 5)
+# print ('Hello world')
+# print ('Hello' * 5)
+
+class Animal:
+
+    _DEGREE_OF_DANGER = 0
+    live = 5
+
+    def attack(self):
+        if self._DEGREE_OF_DANGER >= 5:
+            print("Be careful, I'm attacking you O_O")
+        else:
+            print("Sorry, I'm peaceful :)")
+
+class PoisonousAnimal(Animal):
+
+    _DEGREE_OF_DANGER = 8
+    live = 6
+
+    def attack(self):
+        super().attack()
+
+
+class Duckbill (PoisonousAnimal):
+
+    sound = "Click-click-click"
+
+db = Duckbill()
+db.attack()
+print(db.live)
--- Module_5/module_5_1.py
+++ Module_5/module_5_1.py
@@ -1,9 +1,9 @@
 # Задача "Developer - не только разработчик"
 
 class House:
-    def __init__(self, name, number_of_floors):
+    def __init__(self, name, floors):
         self.name = name
-        self.number_of_floors = number_of_floors
+        self.number_of_floors = floors
 
     def go_to(self, new_floor):
         if (new_floor > self.number_of_floors) or (new_floor < 1):
