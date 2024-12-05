4433dbc67de9001f051ab99d561e4e68d714af48
Alex <e_fridman@mail.ru>
2024-12-05 19:28:33 +0300

    05/12

--- Module_9/module_9_6.py
+++ Module_9/module_9_6.py
@@ -9,13 +9,18 @@
 Вызовите функцию all_variants и выполните итерации.
 """
 
-def all_variants(text):
-    for k in range(len(text)):
-        for j in range(len(text) - k):
-            txt = text[k:j + k + 1]
-            yield txt
+from itertools import combinations
 
-# test:
+def all_variants(text):
+    for k in range(1, len(text) + 1):
+        txt_lst = []
+        b = combinations(text, k)
+        txt_lst.extend(b)
+        for j in txt_lst:
+            j = list(j)
+            yield (''.join(j))
+
+
 a = all_variants("abc")
 for i in a:
     print(i)
