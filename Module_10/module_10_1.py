67a2ff9b610228a67c40d64a36dedf064cac1367
Alex <e_fridman@mail.ru>
2024-12-11 19:20:57 +0300

    11/12

--- Module_10/module_10_1.py
+++ Module_10/module_10_1.py
@@ -21,10 +21,11 @@ def write_words(word_count, file_name):
         for i in range(1, word_count+1):
             file.write(f'Какое-то слово № {i}\n')
             sleep(0.1)
+        print(f'Завершилась запись в файл {file_name}')
 
 startTime = datetime.now()
 write_words(10, 'example1.txt')
-write_words(30,' example2.txt')
+write_words(30,'example2.txt')
 write_words(200, 'example3.txt')
 write_words(100, 'example4.txt')
 endTime = datetime.now()
